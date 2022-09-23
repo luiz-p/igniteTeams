a6fd61ccd08647355bfaf618a67a9d49d2c45ecf
Luiz Pires <lpiresjunior@gmail.com>
2022-09-23 06:33:50 -0300

    handle app errors

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -7,6 +7,8 @@ import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
+import { Alert } from 'react-native';
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
@@ -15,10 +17,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if(group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da Turma.');
+      }
+
       await groupCreate(group);
       navigation.navigate('players', { group });
     } catch (error) {
-      //
+      if(error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+      }
     }
   }
 
